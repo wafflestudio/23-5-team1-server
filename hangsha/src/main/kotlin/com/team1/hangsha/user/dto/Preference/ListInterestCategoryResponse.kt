3bfb9c989e1e624559b34d1c90dbf4027910f886
Lee Seunghyeon <lsh09130@gmail.com>
2026-02-02 00:02:03 +0900

    fix: add @Schema tag for Swagger doc

--- hangsha/src/main/kotlin/com/team1/hangsha/user/dto/Preference/ListInterestCategoryResponse.kt
+++ hangsha/src/main/kotlin/com/team1/hangsha/user/dto/Preference/ListInterestCategoryResponse.kt
@@ -1,10 +1,12 @@
 package com.team1.hangsha.user.dto.Preference
 
 import com.team1.hangsha.category.dto.core.CategoryDto
+import io.swagger.v3.oas.annotations.media.Schema
 
 data class ListInterestCategoryResponse(
     val items: List<Item>
 ) {
+    @Schema(name = "InterestCategoryItem")
     data class Item(
         val category: CategoryDto,
         val priority: Int
